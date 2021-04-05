3128df6bfb327fcde0e74d2dd7f163c70c14d0f2
rn <rn@example.com>
2021-04-06 00:52:30 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App.jsx';
-import Example from './components/Test'
+import App from './App.jsx';
 
-ReactDOM.render(<Example />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
